3a77e77203b7ccba18cfe352dca47fb5fc319162
ゆきかご <p.u.r.u.h.i.m.e@gmail.com>
2014-11-19 20:43:35 +0900

    UnitGroup.js：first commit

---
+++ jasmine-standalone-2.0.1/spec/ActionQueueSpec.js
@@ -0,0 +1,9 @@
+describe('ActionQueue', function(){
+    var unit;
+    var queue;
+
+    beforeEach(function(){
+        unit = new Unit('rika');
+        queue = new ActionQueue();
+    });
+});
---
+++ jasmine-standalone-2.0.1/spec/UnitGroupSpec.js
@@ -0,0 +1,4 @@
+describe('UnitGroup', function(){
+    //TODO ユニットの追加
+    //TODO ユニットの削除
+});
