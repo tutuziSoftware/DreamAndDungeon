21f3814e79cf6f76faabf65147c86c09acfb0905
ゆきかご <p.u.r.u.h.i.m.e@gmail.com>
2014-11-30 19:50:19 +0900

    街で出来る事の詳細に遷移出来るよう修正

--- town.js
+++ town.js
@@ -1,22 +1,43 @@
 var DAD = angular.module('DAD',[]);
 
 DAD.controller('menuController', function($scope){
-    $scope.menuList = [
-        {
+    $scope.menuList = {
+        'yadoya':{
             name:'やどや',
-            id:'yadoya'
+            id:'yadoya',
+            next:{
+                'yadoya_gohan':{
+                    name:'ごはんをたべる',
+                    id:'yadoya_gohan'
+                }
+            }
         },
-        {
+        'douguya':{
             name:'どうぐや',
             id:'douguya'
         },
-        {
+        'guild':{
             name:'ギルド',
             id:'guild'
         },
-        {
+        'sansaku':{
             name:'さんさく',
             id:'sansaku'
         }
-    ];
+    };
+
+    var DEFAULT_MENU = {
+        name:'',
+        id:''
+    };
+
+    $scope.selectedMenu = DEFAULT_MENU;
+
+    $scope.selectMenu = function(id){
+        $scope.selectedMenu = $scope.menuList[id];
+    };
+
+    $scope.showMenu = function(){
+        return $scope.selectedMenu != DEFAULT_MENU;
+    };
 });
