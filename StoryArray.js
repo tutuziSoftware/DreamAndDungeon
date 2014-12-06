cd1db77ab209ec45b1278d2930e0a3e89fe5cb86
ゆきかご <p.u.r.u.h.i.m.e@gmail.com>
2014-12-06 23:56:25 +0900

    ストーリー：ストーリーの開始を実装

--- StoryArray.js
+++ StoryArray.js
@@ -1,3 +1,45 @@
-/**
- * Created by puruhime on 2014/12/06.
- */
+function StoryArray(){
+    this._storys = {
+        'id':{
+        }
+    };
+
+    if(localStorage['storys'] == void 0 || localStorage['storys'] == null){
+        localStorage['storys'] = JSON.stringify({});
+    }
+    this._status = JSON.parse(localStorage['storys']);
+
+    this._updateStatus();
+}
+
+StoryArray.prototype = {
+    start:function(id){
+        if(id in this._storys){
+            this._status[id] = id;
+            this._update();
+
+            this._branches[id] = this._storys[id];
+            this._updateStatus();
+        }
+    },
+    get branches(){
+        return this._branches;
+    },
+    getBranch:function(id){
+        return this._branches[id];
+    },
+    _update:function(){
+        if(localStorage['storys'] == void 0 || localStorage['storys'] == null){
+            localStorage['storys'] = JSON.stringify({});
+        }
+        localStorage['storys'] = JSON.stringify(this._status);
+    },
+    _updateStatus:function(){
+        var branchIds = Object.keys(this._status);
+        this._branches = {};
+        branchIds.forEach(function(branchId){
+            this._branches[branchId] = this._storys[branchId];
+        }, this);
+        this._branches.length = branchIds.length;
+    }
+};
--- jasmine-standalone-2.0.1/spec/StoryArraySpec.js
+++ jasmine-standalone-2.0.1/spec/StoryArraySpec.js
@@ -1,10 +1,21 @@
 describe('ストーリーのフラグ的なものを管理する', function(){
-    beforeEach(function(){
+    var storys;
 
+    beforeEach(function(){
+        delete localStorage['storys'];
+        storys = new StoryArray;
     });
 
-    it('ストーリーを立てる');
-    it('ストーリーを複数立てる');
+    it('現在遂行中のストーリーを取得する', function(){
+        expect(storys.branches.length).toBe(0);
+    });
+    it('新しいストーリー(分岐なし)を実装する', function(){
+        storys.start('id');
+
+        expect(storys.branches.length).toBe(1);
+        expect(storys.getBranch('id')).not.toBeUndefined();
+    });
+    it('新しいストーリー(分岐あり)を実装する');
     it('特定のストーリーが完了したら発生するストーリーを立てる');
     it('特定のストーリーが完了したらストーリーを開始出来ないようにする');
 });
