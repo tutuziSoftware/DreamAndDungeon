9de15c4a7613a9870f3bccdb512b77464c91a65b
ゆきかご <p.u.r.u.h.i.m.e@gmail.com>
2014-12-08 22:18:32 +0900

    ストーリー：データ構造を決定

--- StoryArray.js
+++ StoryArray.js
@@ -1,6 +1,24 @@
+/**
+ * ストーリーを円状に展開する為のクラスです。
+ * このクラスは基本的にただのフラグの集合体です。
+ *
+ * このクラスが通常のフラグと違うのは、
+ * ID「''」から始まりID「''」に終わる円状のフラグリストであるという事です。
+ * @constructor
+ */
 function StoryArray(){
     this._storys = {
         'id':{
+        },
+        'id2-1-s':{
+            next:'id2-1-e'
+        },
+        'id2-1-e':{
+            prev:'id2-1-s',
+            next:'id2-2'
+        },
+        'id2-2':{
+            prev:'id2-1-e'
         }
     };
 
@@ -8,38 +26,54 @@ function StoryArray(){
         localStorage['storys'] = JSON.stringify({});
     }
     this._status = JSON.parse(localStorage['storys']);
-
-    this._updateStatus();
+    this._status[''] = {};
 }
 
 StoryArray.prototype = {
+    /**
+     * ストーリーを開始します。
+     * @param id
+     */
     start:function(id){
-        if(id in this._storys){
-            this._status[id] = id;
-            this._update();
-
-            this._branches[id] = this._storys[id];
-            this._updateStatus();
+        if(id in this._storys === false) return false;
+        if(this._storys[id].prev === void 0){
+            this._storys[id].prev = '';
         }
+
+        if(this._storys[id].prev in this._status === false) return false;
+
+        this._status[id] = this._storys[id];
+        this._update();
+        return true;
+    },
+    /**
+     * ストーリーを進めます。
+     * @param id
+     */
+    next:function(id){
+        if(this._storys[id].next === void 0 || this._storys[id].next === '') {
+            delete this._status[id];
+            this._update();
+            return true;
+        }
+
+        this.start(this._status[id].next);
+        delete this._status[id];
+        this._update();
+        return true;
     },
     get branches(){
-        return this._branches;
+        return {
+            length:Object.keys(this._status).length - 1
+        };
     },
     getBranch:function(id){
-        return this._branches[id];
+        return this._status[id];
     },
     _update:function(){
         if(localStorage['storys'] == void 0 || localStorage['storys'] == null){
             localStorage['storys'] = JSON.stringify({});
         }
         localStorage['storys'] = JSON.stringify(this._status);
-    },
-    _updateStatus:function(){
-        var branchIds = Object.keys(this._status);
-        this._branches = {};
-        branchIds.forEach(function(branchId){
-            this._branches[branchId] = this._storys[branchId];
-        }, this);
-        this._branches.length = branchIds.length;
     }
 };
--- jasmine-standalone-2.0.1/spec/StoryArraySpec.js
+++ jasmine-standalone-2.0.1/spec/StoryArraySpec.js
@@ -9,13 +9,33 @@ describe('ストーリーのフラグ的なものを管理する', function(){
     it('現在遂行中のストーリーを取得する', function(){
         expect(storys.branches.length).toBe(0);
     });
-    it('新しいストーリー(分岐なし)を実装する', function(){
+    it('新しいストーリーを開始する', function(){
         storys.start('id');
 
         expect(storys.branches.length).toBe(1);
         expect(storys.getBranch('id')).not.toBeUndefined();
     });
-    it('新しいストーリー(分岐あり)を実装する');
+    it('新しいストーリーを開始し、シーンごとにフラグを進めていきます', function(){
+        //開始
+        storys.start('id2-1-s');
+        expect(storys.branches.length).toBe(1);
+
+        //prevがあるので新しいストーリーとして追加出来ない
+        storys.start('id2-2');
+        expect(storys.branches.length).toBe(1);
+
+        storys.next('id2-1-s');
+        expect(storys.branches.length).toBe(1);
+        //TODO 次のストーリーのIDを返す
+
+        storys.next('id2-1-e');
+        expect(storys.branches.length).toBe(1);
+        //TODO 次のストーリーのIDを返す
+
+        //最後のストーリーを進めたので、ストーリー消化となる
+        storys.next('id2-2');
+        expect(storys.branches.length).toBe(0);
+    });
     it('特定のストーリーが完了したら発生するストーリーを立てる');
     it('特定のストーリーが完了したらストーリーを開始出来ないようにする');
 });
